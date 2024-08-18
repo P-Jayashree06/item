0afe2dab542e26127abed23ac47be5d2a6550d1b
P-Jayashree06 <jayashree122005@gmail.com>
2024-08-18 19:59:47 +0530

    Add files via upload

---
+++ item.java
@@ -0,0 +1,7 @@
+public class item {
+    public static void main(String[] args) {
+        System.out.println("Item;item1  Quantity:10  Price:$15.99");
+        System.out.println("Item;item2  Quantity:20  Price:$25.99");
+        System.out.println("Item;item3  Quantity:30  Price:$35.99");
+    }
+}
